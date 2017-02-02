18de37a0c170bca3fa7cb54444a9b33322ae4e7a
zpc <770651352@qq.com>
2017-02-02 23:31:35 +0800

    添加微博

--- Modules/Home/Resources/views/index/weibo_info.blade.php
+++ Modules/Home/Resources/views/index/weibo_info.blade.php
@@ -3,7 +3,7 @@
         <i class="fa fa-heart" aria-hidden="true"></i>微博最新动态
         <small>Weibo Changes</small>
     </h4>
-    <div>
-
+    <div class="weibo-details">
+        <iframe id="weibo" style="width:100%; height:500px;" frameborder="0" scrolling="no" src="http://v.t.sina.com.cn/widget/widget_blog.php?uid=1745184020"></iframe>
     </div>
 </div>
